8bbfe1d2308e614322d2183f5c1a31005b948c1e
景賦 邱 <jerry384052@gmail.com>
2022-06-12 13:30:07 +0800

    modify readme and code

--- JlizBankMvc/Models/SendCodeMailService.cs
+++ JlizBankMvc/Models/SendCodeMailService.cs
@@ -4,45 +4,43 @@ using System.Net.Mail;
 
 namespace JlizBankMvc.Models
 {
-    public class SendCodeMailService
-    {
-        public string GetVerificationCode(string Email)
-        {
-            //產生驗證碼
-            string codes = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            Random random = new Random();
-            string verifyCode = string.Empty;
-            for (int i = 0; i < 8; i++)
-            {
-                int index = random.Next(codes.Length);
-                verifyCode += codes[index];
-            }
-            //發送Email驗證碼
-            using (MailMessage mailMessage = new MailMessage())
-            {
-                //設定發送者
-                mailMessage.From = new MailAddress("jerry384052@gmail.com");
-                //接收人
-                mailMessage.To.Add(Email);
-                mailMessage.Subject = "傑力士商業銀行---重新設定密碼--驗證碼";
-                mailMessage.Body = $"您的驗證碼為 {verifyCode}";
+	public class SendCodeMailService
+	{
+		public string GetVerificationCode(string Email)
+		{
+			//Generate the random code
+			string codes = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			Random random = new Random();
+			string verifyCode = string.Empty;
+			for (int i = 0; i < 8; i++)
+			{
+				int index = random.Next(codes.Length);
+				verifyCode += codes[index];
+			}
+			//Send Email
+			using (MailMessage mailMessage = new MailMessage())
+			{
+				//Set receiver
+				mailMessage.From = new MailAddress("jerry384052@gmail.com");
+				//Receiver
+				mailMessage.To.Add(Email);
+				mailMessage.Subject = "Jliz Internet Bank-Reset Password--VerificationCode";
+				mailMessage.Body = $"Your code are {verifyCode}";
 
-                //使用 SMTP 寄送 Email
-                using (SmtpClient smtp = new SmtpClient())
-                {
-                    smtp.Credentials = new NetworkCredential("jerry384052@gmail.com", "xiwjmiggmzfbodvr");
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.Port = 25;
-                    smtp.EnableSsl = true;
-                    smtp.Send(mailMessage);
+				//Use SMTP to send Email
+				using (SmtpClient smtp = new SmtpClient())
+				{
+					smtp.Credentials = new NetworkCredential("jerry384052@gmail.com", "xiwjmiggmzfbodvr");
+					smtp.Host = "smtp.gmail.com";
+					smtp.Port = 25;
+					smtp.EnableSsl = true;
+					smtp.Send(mailMessage);
+				}
+			}
+			return verifyCode;
+		}
 
-                }
-
-            }
-            return verifyCode;
-        }
-
-    }
+	}
 }
 
 
